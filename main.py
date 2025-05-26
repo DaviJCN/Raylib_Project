fb846d8a51225b93c89cfbd7ddf50a17a1a16c00
Davi Nascimento <djcnunb@gmail.com>
2025-05-26 16:17:14 -0300

    Correção na taxa de atualização e inputs do teclado

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class Game:
         self.jogador = pygame.transform.scale(
             self.jogador, (60, 60)
         )  # Aumenta a imagem para 60x60
+        self.velocidade_jogador = 500 # Define a velocidade do player
         self.jogador_rect = self.jogador.get_frect(
             center=(
                 self.width / 2,
@@ -27,31 +28,30 @@ class Game:
         self.run()  # Inicializa a funçao run, para rodar o jogo
 
     def run(self):
-        dt = (
-            self.clock.tick(60) / 1000
-        )  # Variavel Delta Time, usada para funções de movimento e para limitar o fps do jogo
         while self.running:  # Loop do jogo
             # Verifica se tentou fechar a janela, caso sim, ela fecha
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                dt = (self.clock.tick(144) / 1000)  # Variavel Delta Time, usada para funções de movimento e para limitar o fps do jogo (dentro do loop para atualizar a taxa de fps junto com o loop)
             self.mouse()  # Chama a função
             self.display_surface.fill("gray33")
             self.display_surface.blit(self.jogador, self.jogador_rect)
             self.input(dt)
             self.anti_bobby_collision()
             pygame.display.update()
+            
 
     def input(self, dt):  # Input WASD Básico
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            self.jogador_rect.centery -= 1
+            self.jogador_rect.centery -= self.velocidade_jogador * dt
         if keys[pygame.K_s]:
-            self.jogador_rect.centery += 1
+            self.jogador_rect.centery += self.velocidade_jogador * dt
         if keys[pygame.K_a]:
-            self.jogador_rect.centerx -= 1
+            self.jogador_rect.centerx -= self.velocidade_jogador * dt
         if keys[pygame.K_d]:
-            self.jogador_rect.centerx += 1
+            self.jogador_rect.centerx += self.velocidade_jogador * dt
 
     def mouse(self):
         pygame.mouse.set_pos(
@@ -62,11 +62,11 @@ class Game:
             pygame.mouse.get_just_pressed()
         )  # variavel de entrada e saida dos clicks do mouse
         if self.clicado[0]:
-            print("asbon")
+            print("Left")
         if self.clicado[2]:
-            print("asboner")
+            print("Right")
 
-    def anti_bobby_collision(self):
+    def anti_bobby_collision(self): # Define a colisão do antibobby
         if self.jogador_rect.bottom > self.height:
             self.jogador_rect.bottom = self.height
 
@@ -78,6 +78,6 @@ class Game:
 
         if self.jogador_rect.left < 0:
             self.jogador_rect.left = 0
-
-
+            
+        
 Game()
