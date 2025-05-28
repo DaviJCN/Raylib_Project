af184d46db06862bc85f589b6417d645db11df36
Pekrio <pekriopedro@gmail.com>
2025-05-28 20:28:32 -0300

    Bugfix e Documentação Consertei o set mouse visible, colocando a opção para falso e comentei a função input inteira pra NINGUEM ficar chorando depois.

--- main.py
+++ main.py
@@ -46,24 +46,30 @@ class Game:
 
     def input(self, dt):  # Input WASD Básico
         keys = pygame.key.get_pressed()
-        self.jogador_direcao[0] = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-        self.jogador_direcao[1] = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.jogador_direcao[0] = (
+            int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        )  # Se o jogador vai direita a direção é 1, se vai pra esquerda é -1, parado é 0
+        self.jogador_direcao[1] = int(keys[pygame.K_s]) - int(
+            keys[pygame.K_w]
+        )  # Mesma coisa, so que pro eixo y
         self.jogador_direcao = (
             self.jogador_direcao.normalize()
             if self.jogador_direcao
             else self.jogador_direcao
-        )
+        )  # "Normaliza" a direção do jogador, fazendo o numero ficar entre 1 e -1, para nao alterar a velocidade do mesmo
         self.jogador_rect.centerx += (
             self.velocidade_jogador * dt * self.jogador_direcao[0]
-        )
+        )  # O jogador so mexe se a direção for diferente de 0
         self.jogador_rect.centery += (
             self.velocidade_jogador * dt * self.jogador_direcao[1]
         )
 
-    def mouse(self): # Rastreia o mouse
-        mouse_x, mouse_y = pygame.mouse.get_pos() # Obtém a posição atual do mouse a cada frame
-        print(mouse_x, mouse_y) # Escreve a posição do mouse
-        pygame.mouse.set_visible(True)  # Define o mouse como invisível
+    def mouse(self):  # Rastreia o mouse
+        mouse_x, mouse_y = (
+            pygame.mouse.get_pos()
+        )  # Obtém a posição atual do mouse a cada frame
+        print(mouse_x, mouse_y)  # Escreve a posição do mouse
+        pygame.mouse.set_visible(False)  # Define o mouse como invisível
         self.clicado = (
             pygame.mouse.get_just_pressed()
         )  # variavel de entrada e saida dos clicks do mouse
@@ -84,11 +90,9 @@ class Game:
 
         if self.jogador_rect.left < 0:
             self.jogador_rect.left = 0
-            
-    def range_anti_bobvy(self):
-        pass
-    
 
+    def range_anti_bobvy(self):
+        pass  # Fez a boa colocando uma função que nao faz NADA!!!
 
 
 Game()
